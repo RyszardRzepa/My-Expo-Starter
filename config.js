c5a3b305ff03d9e8d2b2ea4be564d05346d3009d
ryszard rzepa <ryszardrzepa@ryszards-MacBook-Pro.local>
2017-05-29 10:21:21 +0200

    i

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey : 'AIzaSyBaB_6jFIUG7q8lpYHbxvtsNzyVr0xDuXA',
+  authDomain : 'weather-forecast-9a2a5.firebaseapp.com',
+  databaseURL: "https://weather-forecast-9a2a5.firebaseio.com",
+  projectId: "weather-forecast-9a2a5",
+  storageBucket: "weather-forecast-9a2a5.appspot.com",
+  messagingSenderId: "317335334484"
+};
+
+export default config;
